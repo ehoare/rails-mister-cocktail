6bbcf6b98b9cc2520785cb10785d794428c9dd02
Eric Hoare <hoareinc@gmail.com>
2021-02-19 16:08:34 -0300

    cambio production.rb

--- config/application.rb
+++ config/application.rb
@@ -23,11 +23,7 @@ module RailsMisterCocktail
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    config.assets.initialize_on_precompile = false
-    # config.assets.compile = false
-
-
+    
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
